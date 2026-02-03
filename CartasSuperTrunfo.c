a533f6035cf860ba821d0a14604ced321a53b3ea
Alessandro Sampaio <alessandrosampaio843@gmail.com>
2026-02-03 18:32:27 +0000

    criei os printf da carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,12 +25,15 @@ int main() {
 
 
   printf("Carta 1:\n");
-  printf("populacao: %d\n" , A01.Populacao);
+  printf("Populacao: %d\n" , A01.Populacao);
   printf("Area: %.2f\n" , A01.Area);
   printf("PIB: %.2f\n" , A01.PIB);
   printf("numero de Pontos Turistico: %d\n", A01.Numero_de_Pontos_Turistico);
-  
 
- 
+  printf("carta 2:\n");
+  printf("Populacao: %d\n" , B02.Populacao);
+  printf("Area: %.2f\n" , B02.Area);
+  printf("PIB: %.2f\n" , B02.PIB);
+  printf("Numero de Pontos Turistico: %d\n" , B02.Numero_de_Pontos_Turistico)
 return 0;
 } 

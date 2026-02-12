1596171215286d96fb105dc661a19a811d13b11d
Alessandro Sampaio <alessandrosampaio843@gmail.com>
2026-02-12 13:57:38 +0000

    Coloquei no calculo do super poder o Float para converter os inteiros e 1.0f / densidade quanto menor a densidade maior seja o valor somado usei o <(menor que) e >(maior que)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,8 @@ typedef struct
   float PIB;
   int Numero_de_Pontos_Turistico; 
   float Densidade_Populacional;
-  float PIB_per_Capita; // Corrigido de Capital para Capita
+  float PIB_per_Capita;
+  float Super_Poder; // Novo atributo
 } Carta;
 
 int main() {
@@ -15,39 +16,48 @@ int main() {
   Carta A01 = {
     1996400,
     435.5,
-    12500.0,
-    35};
+    12500.0, 
+    35,
+  };
   Carta B02 = {
     508826,
-    675.4, 
-    8000.0, 
-    100};
+    675.4,
+    8000.0,
+    100,
+  };
 
   // --- Cálculos para a Carta A01 ---
   A01.Densidade_Populacional = (float)A01.Populacao / A01.Area;
   A01.PIB_per_Capita = A01.PIB / (float)A01.Populacao;
+  // Super Poder: Soma de tudo, mas usamos o inverso da densidade (1/densidade)
+  A01.Super_Poder = (float)A01.Populacao + A01.Area + A01.PIB + 
+                    (float)A01.Numero_de_Pontos_Turistico + A01.PIB_per_Capita + 
+                    (1.0f / A01.Densidade_Populacional);
 
   // --- Cálculos para a Carta B02 ---
   B02.Densidade_Populacional = (float)B02.Populacao / B02.Area;
   B02.PIB_per_Capita = B02.PIB / (float)B02.Populacao;
+  B02.Super_Poder = (float)B02.Populacao + B02.Area + B02.PIB + 
+                    (float)B02.Numero_de_Pontos_Turistico + B02.PIB_per_Capita + 
+                    (1.0f / B02.Densidade_Populacional);
 
-  // Exibição da Carta 1
-  printf("Carta 1:\n");
-  printf("Populacao: %d\n" , A01.Populacao);
-  printf("Area: %.2f km²\n" , A01.Area);
-  printf("PIB: %.2f\n" , A01.PIB);
-  printf("Pontos Turisticos: %d\n", A01.Numero_de_Pontos_Turistico);
-  printf("Densidade Populacional: %.2f hab/km²\n", A01.Densidade_Populacional);
-  printf("PIB per Capita: %.4f\n", A01.PIB_per_Capita);
+  // Exibição das Cartas (Opcional, mas útil para conferir)
+  printf("Carta 1 - Poder: %.2f | Carta 2 - Poder: %.2f\n\n", A01.Super_Poder, B02.Super_Poder);
 
-  // Exibição da Carta 2
-  printf("Carta 2:\n");
-  printf("Populacao: %d\n" , B02.Populacao);
-  printf("Area: %.2f km²\n" , B02.Area);
-  printf("PIB: %.2f\n" , B02.PIB);
-  printf("Pontos Turisticos: %d\n" , B02.Numero_de_Pontos_Turistico);
-  printf("Densidade Populacional: %.2f hab/km²\n", B02.Densidade_Populacional);
-  printf("PIB per Capita: %.4f\n", B02.PIB_per_Capita);
+  // --- COMPARAÇÃO E EXIBIÇÃO DOS RESULTADOS ---
+  // Imprime 1 se Carta 1 vence, 0 se Carta 2 vence
+  printf("Comparacao de Cartas (1 = Carta 1 vence, 0 = Carta 2 vence):\n");
+  
+  printf("Populacao: %d\n", A01.Populacao > B02.Populacao);
+  printf("Area: %d\n", A01.Area > B02.Area);
+  printf("PIB: %d\n", A01.PIB > B02.PIB);
+  printf("Pontos Turisticos: %d\n", A01.Numero_de_Pontos_Turistico > B02.Numero_de_Pontos_Turistico);
+  
+  // Regra especial: Menor densidade vence
+  printf("Densidade Populacional: %d\n", A01.Densidade_Populacional < B02.Densidade_Populacional);
+  
+  printf("PIB per Capita: %d\n", A01.PIB_per_Capita > B02.PIB_per_Capita);
+  printf("Super Poder: %d\n", A01.Super_Poder > B02.Super_Poder);
 
   return 0;
 }
